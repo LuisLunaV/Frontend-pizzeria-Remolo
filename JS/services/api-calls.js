0984db3192a4b71841c127a60b687f96fa9f7c91
LuisLunaV <luis.a.luna.v@outlook.com>
2023-01-21 02:11:02 -0600

    Se agrega la url api en el archivo services.js

---
+++ JS/services/api-calls.js
@@ -0,0 +1,11 @@
+//URL de la api que vamos a utilizar.
+const url = 'https://backend-pizzeria-remolo-production.up.railway.app';
+
+//Guardamos en un objeto las rutas que vamos a utilizar en la api.
+const { usuarios, categorias, productos, pedidos } = {
+    usuarios:  '/api/usuarios',
+    categorias:'/api/categorias',
+    productos: '/api/productos',
+    pedidos:   '/api/pedidos'
+};
+
