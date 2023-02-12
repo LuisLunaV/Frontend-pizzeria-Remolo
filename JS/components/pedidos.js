738199213f6808a682008fc8cd21c6dda151ab0f
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-12 03:58:26 -0600

    Se agrega los precios a los pedidos

---
+++ JS/components/pedidos.js
@@ -0,0 +1,28 @@
+const modal = document.querySelector(".productos");
+
+export const htmlPedido = ({Prod_id, Prod_Nombre, Prod_categoriaID, Prod_Descripcion, Prod_imagen, precio_Unitario }) =>{
+    
+    const html = `
+    <div class="card" data-value='${ Prod_categoriaID }'>
+    <div class="card-img" >
+      <img src="${Prod_imagen}" alt="img_producto">
+    </div>
+
+    <div class="card-contenido" data-value='${ Prod_id }'>
+    <div class="card-info">
+      <p class="text-title">${Prod_Nombre}</p>
+      <p class="text-body">${Prod_Descripcion}</p>
+    </div>
+    <div class="card-footer">
+    <span class="text-title">$${ precio_Unitario }</span>
+    <div class="card-button-agregar">
+    <input class='button-eliminar' type="button" value="Eliminar" />
+  </div>
+  </div>
+
+</div>
+</div>
+    `;
+
+modal.innerHTML += html;
+};
