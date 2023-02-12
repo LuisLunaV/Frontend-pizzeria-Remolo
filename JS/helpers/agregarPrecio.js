636eb8a789c8887a5f78f0baa992acf4a7717e1b
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-12 00:57:15 -0600

    Se agrega funcion agregar precio al producto

---
+++ JS/helpers/agregarPrecio.js
@@ -0,0 +1,27 @@
+import { getPrecios } from '../index.js';
+
+export const agregarPrecio = async( producto )=>{
+    const { precio } = await getPrecios();
+
+    /**
+     * Utilizamos una estructura de datos tipo dicionarioa para agregar los precios a
+     * los productos.
+     * Este código crea un mapa con los precios asociados a cada identificador de 
+     * producto, y luego utiliza el método map para recorrer el arreglo de productos y 
+     * agregar el precio correspondiente a cada producto en el nuevo arreglo 
+     * productosConPrecio.
+     */
+    const mapPrecio = await precio.reduce(( acc, precio ) => {
+        acc[precio.Precio_ProdID] = precio.Precio_Unitario;
+        return acc;
+    }, {});
+
+    const pedidosConPrecio = producto.map( producto =>{
+        return{
+            ...producto,
+            precio_Unitario: mapPrecio[ producto.Prod_id ]
+        }
+    });
+
+    return pedidosConPrecio;
+};
