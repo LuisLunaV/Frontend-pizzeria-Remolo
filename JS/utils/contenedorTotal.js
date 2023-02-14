67339049e01bd57081398abf0ed26c07a0a38e69
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-14 01:08:48 -0600

    Se agerga un setTimeOut

---
+++ JS/utils/contenedorTotal.js
@@ -0,0 +1,19 @@
+const contenedorDelTotal = document.querySelector('.total-productos');
+
+const mostrarContenedorTotal =()=>{
+    contenedorDelTotal.classList.remove('ocultar-total');
+    contenedorDelTotal.classList.add('mostrar-total');
+};
+
+const ocultarContenedorTotal =()=>{
+    setTimeout(()=>{
+        contenedorDelTotal.classList.add('ocultar-total');
+        contenedorDelTotal.classList.remove('mostrar-total');
+    },500)
+    
+};
+
+export{
+    mostrarContenedorTotal,
+    ocultarContenedorTotal
+}
