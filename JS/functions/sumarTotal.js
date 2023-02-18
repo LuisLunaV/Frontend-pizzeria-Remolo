06e817d0f692ceaa7bb4a1ac7f3db1fa7f099e7b
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-17 22:39:27 -0600

    Se agrega carpeta para refatorizar codigo a funciones independientes

---
+++ JS/functions/sumarTotal.js
@@ -0,0 +1,8 @@
+const sumarTotal = ( cantidad ) =>{
+const pedidos = JSON.parse( localStorage.getItem('pedidos'));
+pedidos.forEach(({PD_Cantidad, }) => {
+    console.log(element)
+});
+};
+
+// sumarTotal()
