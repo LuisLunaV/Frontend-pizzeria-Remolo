d402d11fbd2a974eb77fbe5ef8e9288d7bd520dc
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-17 23:51:57 -0600

    Creamos el algoritmos para sumar los totales de los pedidos

--- JS/functions/sumarTotal.js
+++ JS/functions/sumarTotal.js
@@ -1,8 +1,24 @@
-const sumarTotal = ( cantidad ) =>{
-const pedidos = JSON.parse( localStorage.getItem('pedidos'));
-pedidos.forEach(({PD_Cantidad, }) => {
-    console.log(element)
-});
-};
+import { htmlTotal } from '../index.js';
 
-// sumarTotal()
+/**
+ * Obtenemos los precios de los productos seleccionados y guardados en el
+ * localStorage, los sumamos e imprimimos en el DOM.
+ */
+export const sumarTotal = ( ) =>{
+
+const pedidos = JSON.parse( localStorage.getItem('pedidos'));
+
+const precios = [];
+
+pedidos.forEach(({PD_Cantidad, precio_Unitario }) => {
+    precios.push( PD_Cantidad*precio_Unitario);
+});
+
+const total = precios.reduce((acc, valor)=>{
+   return acc = acc + valor
+},0)
+
+//Enviamos la cantidad a imprimir en el DOM.
+htmlTotal( total );
+
+};
