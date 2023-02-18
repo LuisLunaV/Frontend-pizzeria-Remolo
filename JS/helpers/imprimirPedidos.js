0002f510e9dc6d55cc175a7e24cb42fd9b84b165
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-17 23:52:52 -0600

    agregamos la funcion sumarTotal

--- JS/helpers/imprimirPedidos.js
+++ JS/helpers/imprimirPedidos.js
@@ -1,13 +1,19 @@
-import { pedido, agregarPrecio ,htmlPedido } from '../index.js';
+import { pedido, htmlPedido, sumarTotal } from '../index.js';
 
 const modal = document.querySelector(".productos");
 
+/**
+ * Imprimimos los productos del pedido y el total a pagar.
+ */
 export const imprimirPedido = async()=>{
+
     modal.innerHTML = '';
    
     const nuevoPedido = pedido.pedidos;
    
     nuevoPedido.forEach( htmlPedido );
 
+    //Imprimimos el total del pedido.
+    sumarTotal();
 };
 
