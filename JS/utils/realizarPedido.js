ba69b92411f9c7c9fc10f14cb9a9840235426db3
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-20 22:39:25 -0600

    Se agrega nuevo archivo

---
+++ JS/utils/realizarPedido.js
@@ -0,0 +1,16 @@
+import { imprimirFormulario , cerrarModalProductos, abrirModalFormulario } from "../index.js";
+/**
+ * Cuando damos click en el boton "Pagar", llamamos a las funciones
+ * que nos permiten continuar con el pedido.
+ *
+ */
+export const realizarPedido =()=>{
+    
+    const btnPagar = document.querySelector('.btn-succes');
+    
+    btnPagar.addEventListener('click',()=>{
+        cerrarModalProductos();
+        abrirModalFormulario();
+        imprimirFormulario();
+    });
+};
