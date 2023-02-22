db82b54d67e1f964cb3c3a32262fdce4222371ce
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-22 00:30:40 -0600

    Se el argumento precio_unitario a PD_precioUnitario

--- JS/components/pedidos.js
+++ JS/components/pedidos.js
@@ -1,5 +1,5 @@
 const modal = document.querySelector(".productos");
-export const htmlPedido = ({Prod_id, Prod_Nombre, Prod_categoriaID, Prod_Descripcion, Prod_imagen, precio_Unitario, PD_Cantidad }) =>{
+export const htmlPedido = ({Prod_id, Prod_Nombre, Prod_categoriaID, Prod_Descripcion, Prod_imagen, PD_PrecioUnitario, PD_Cantidad }) =>{
     
     const html = `
     <div class="card" data-value='${ Prod_categoriaID }'>
@@ -10,11 +10,11 @@ export const htmlPedido = ({Prod_id, Prod_Nombre, Prod_categoriaID, Prod_Descrip
     <div class="card-contenido" data-value='${ Prod_id }'>
     <div class="card-info">
       <p class="text-title">${Prod_Nombre}</p>
+      <p class="text-title">Cantidad: ${PD_Cantidad}</p>
       <p class="text-body">${Prod_Descripcion}</p>
-      <p class="text-body">Cantidad:${PD_Cantidad}</p>
     </div>
     <div class="card-footer-pedido">
-    <span class="text-title">$${ precio_Unitario }</span>
+    <span class="text-title">$${ PD_PrecioUnitario }</span>
     <div class="card-button-eliminar">
     <input class='button-eliminar' type="button" value="Eliminar" />
   </div>
