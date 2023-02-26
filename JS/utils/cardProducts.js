f39afefce7bded74d8cdc87a353e028815396d71
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-26 14:15:14 -0600

    Se agrega funcion agregarProducto

--- JS/utils/cardProducts.js
+++ JS/utils/cardProducts.js
@@ -1,8 +1,8 @@
-import { pedido, agregarPrecio } from "../index.js";
+import { agregarProducto } from "../index.js";
 
 export const tarjetaProductos = () => {
-  const cardProducto = document.querySelectorAll(".card");
 
+  const cardProducto = document.querySelectorAll(".card");
   /**
    * Realizamos un forEach para seleccionar todas las tarjetas con la clase '.card'
    * y agregarle el evento 'Click' a cada una de ellas.
@@ -38,54 +38,11 @@ export const tarjetaProductos = () => {
       }
 
       if (btnAgregar) {
-        // Obtenemos los lementos que contengan los data-value de categorias y productos.
-        const elementoCateg = event.target.closest(".card");
-        const elementoProd = event.target.closest(".card-contenido");
-
-        // Extraemos los valores "id" de los elementos anteriores.
-        const categID = elementoCateg.dataset.value;
-        const prodID = elementoProd.dataset.value;
-
-        /**
-         * Buscamos la lista de productos por categoria en el localStorage donde se guardo la informacion
-         * de la api.
-         */
-        const listaProd = localStorage.getItem(`producto-${categID}`);
-
-        const { producto } = JSON.parse(listaProd);
-
-        //Buscamos el producto seleccionado
-        const obtenerProducto = producto.filter((producto) => {
-          if (prodID == producto.Prod_id) {
-            return producto;
-          }
-        });
-
-        /**
-         * Destructuramos el objeto del producto obtenido y le agregamos la nueva propiedad "cantidad", donde se
-         * agregara el valor del contador, y devolvemos el objeto.
-         */
-        const productoSinPrecio = obtenerProducto.map((index) => {
-          return {
-            ...index,
-            PD_Cantidad: contador,
-          };
-        });
-
-        //Agregamos el precio al producto seleccionado del pedido
-        const productoConPrecio = await agregarPrecio(productoSinPrecio);
-
-        /**
-         * Realizamos una destructuracion de arreglos para obtener el primer elemento del mismo,
-         * y lo guardamos en la constante "productoGuardado".
-         * Equivale a: const productoGuardado = guardarProducto[0];
-         */
-        const [productoGuardado] = productoConPrecio;
-
-        //Creamos nuestro pedido.
-
-        pedido.nuevoPedido(productoGuardado);
+    
+        agregarProducto( event, contador );
+    
       }
+    
     });
   });
 };
