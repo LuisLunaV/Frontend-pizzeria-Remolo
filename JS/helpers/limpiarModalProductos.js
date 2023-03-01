106b88878c40362f141d486d56da4d456cae7454
LuisLunaV <luis.a.luna.v@outlook.com>
2023-03-01 01:13:06 -0600

    Se agrega el selector del contenedor total

--- JS/helpers/limpiarModalProductos.js
+++ JS/helpers/limpiarModalProductos.js
@@ -1,5 +1,7 @@
 const modal = document.querySelector(".productos");
+const total = document.querySelector('.total-productos');
 
 export const limpiarModalProductos=()=>{
     modal.innerHTML='';
+    total.innerHTML="";
 };
