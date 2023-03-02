41ad0123c9af3539a14a180e532f7c08cf910303
LuisLunaV <luis.a.luna.v@outlook.com>
2023-03-01 22:40:44 -0600

    Se agrega validador de existencia de pedidos

--- JS/utils/modalPedidos.js
+++ JS/utils/modalPedidos.js
@@ -2,35 +2,31 @@ import {
   imprimirPedido,
   mostrarContenedorTotal,
   realizarPedido,
-  abrirModalProductos,
+  abrirModalProductos
 } from "../index.js";
 
-// Elementos del dropdown y el boton principal responsive
-const dropdownMenu = document.querySelector(".dropdown-menu"),
-btnInputResponsive = document.querySelector(".btn-input-responsive");
 
 // Nos ayuda a mostrar los items del dropdown, y los pedidos almacenados.
-
 export const modalPedidos = () => {
-  dropdownMenu.addEventListener("click", (event) => {
-    //El elemento contiene la clase "abrir-modal" True o False
-    const mostrarPedidos = event.target.classList.contains("abrir-modal");
 
-    //Si el elemento es false no hagas nada.
-    if (!mostrarPedidos) return;
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+  
+  dropdownMenu.addEventListener("click", (event) => {
+    /**
+     *Cuando ejecutamos el evento del boton, se hace una nueva busqueda en el localStorage, cada "click"
+     es una busqueda independiente de la anterior. Validamos si hay algun producto en el pedido
+     */
+  const productosDelLocal = JSON.parse(localStorage.getItem('pedidos'));
+  if( !productosDelLocal )return alert('No hay productos en el carrito.');
 
     // ocultamos el menu dropdown
     document.querySelector(".dropdown-menu").classList.add("mostrar-elemento");
 
     //mostramos la venta modal
     abrirModalProductos();
-    mostrarContenedorTotal();
     imprimirPedido();
+    mostrarContenedorTotal();
     realizarPedido();
   });
 
-  //mostrar ventana modal con el boton responsive
-  btnInputResponsive.addEventListener("click", () => {
-    abrirModalProductos();
-  });
 };
