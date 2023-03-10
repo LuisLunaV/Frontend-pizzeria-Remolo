aff7b3c09b41890aea6d0f0f1ab612965af780b2
LuisLunaV <luis.a.luna.v@outlook.com>
2023-03-09 19:21:19 -0600

    ESte archivo contiene los componentes de la alerta del formulario

---
+++ JS/components-alert/alertaSalidaFormulario.js
@@ -0,0 +1,27 @@
+export const alertaSalidaDelFormulario = async() =>{
+   const resp = await Swal.fire({
+        title: "¿Esta seguro que desea salir del formulario?",
+        // text: "Recuerda que puedes cambiar de opinión en cualquier momento antes de confirmar la cancelación",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Salir",
+        cancelButtonText: "No",
+    
+    
+        customClass: {
+          popup: "popup-alert",
+          title: "title-info",
+          icon: "icon-info",
+          htmlContainer: "info-content-alert",
+          confirmButton: 'confirmButtonSucces-alert',
+          cancelButton:'cancelButtonWarning-alert'
+        },
+    
+      }).then((result) => {
+        return result.isConfirmed;
+      });
+
+      return resp;
+};
